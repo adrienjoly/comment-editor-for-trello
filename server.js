f3f1308cdace8d4eaecdf5532aa40bf287539b43
System <none>
2017-07-04 08:55:31 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -13,38 +13,17 @@ var github = githubhook({
 github.listen();
 
 github.on('*', function (event, repo, ref, data) {
-  console.log("Ping!");
-});
+    console.log("Pinged by GitHub!");
+  
+    var sys  = require('util'),
+        exec = require('child_process').exec,
+        child;
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    child = exec('sh git.sh', function (error, stdout, stderr){
+        if (error){ // There was an error executing our script
+          console.err(error);
+        } else { // Script ran ok
+          console.log("git.sh run ok: ", stdout);
+        }
+    });  
 });
