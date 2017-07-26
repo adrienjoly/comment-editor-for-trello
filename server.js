2fe1643e595aa94d7c68b3860e2847c8a8406b9e
Adrien Joly <adrien@whyd.com>
2017-07-26 18:03:37 +0200

    trying to use an express middleware for githubhook

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 // init project
+/*
 var githubhook = require('githubhook');
 var github = githubhook({
   port: process.env.PORT,
   secret: process.env.GITHUB_WEBHOOK_SECRET
 });
+*/
 // TODO: use express-github-webhook, for express ?
 
-github.listen();
+const express = require('express');
+const bodyParser = require('body-parser');
+const GithubWebHook = require('express-github-webhook');
+const github = GithubWebHook({
+  path: '/github/callback', // for compatibility with npm:githubhook
+  secret: process.env.GITHUB_WEBHOOK_SECRET
+});
+
+// use in your express app 
+let app = express();
+app.use(bodyParser.json()); // must use bodyParser in express 
+app.use(github); // use our middleware 
+
+//github.listen();
 
 github.on('push:glitch-github-sync', function (event, repo, ref, data) {
     console.log("Received a push from GitHub!");
@@ -22,4 +37,8 @@ github.on('push:glitch-github-sync', function (event, repo, ref, data) {
           console.log("git.sh ran ok: ", stdout);
         }
     });  
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log('Server up and running 🏃');
 });
