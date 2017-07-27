7a2601cd4e396af379fbc2972f7f66b1096c423e
Adrien Joly <adrien@whyd.com>
2017-07-27 15:13:22 +0200

    bug fix: pass token and commentId to js file

--- public/js/comment-editor.js
+++ public/js/comment-editor.js
@@ -1,32 +1,39 @@
-var $editor = $('.mod-comment-type');
-var $input = $('.js-new-comment-input');
-var $lastSavedTime = $('.date > span');
-var $save = $('.js-save-edit');
+function initEditor(token, commentId) {
 
-function refreshLastSavedTime(date) {
-  $lastSavedTime.text(new Date(date || new Date()).toLocaleTimeString());
-}
+  var $editor = $('.mod-comment-type');
+  var $input = $('.js-new-comment-input');
+  var $lastSavedTime = $('.date > span');
+  var $save = $('.js-save-edit');
 
-$('.js-save-edit').click(function() {
-  var value = $input.val();
-  console.log('saving', value, '...');
-  $editor.addClass('is-saving');
-  $save.attr('disabled', '');
-  $.post('/save', { token: '{{token}}', commentId: '{{comment.id}}', value: value })
-  .then(function(res){
-    console.log('=>', res);
-    // TODO: make sure that res.ok
-    $editor.removeClass('has-changed');
-    refreshLastSavedTime();
-  }, function() {
-    console.log('=> FAIL:', arguments);
-  }).always(function() {
-    $editor.removeClass('is-saving');
-    $save.attr('disabled', null);
+  function refreshLastSavedTime(date) {
+    $lastSavedTime.text(new Date(date || new Date()).toLocaleTimeString());
+  }
+
+  $('.js-save-edit').click(function() {
+    var value = $input.val();
+    console.log('saving', value, '...');
+    $editor.addClass('is-saving');
+    $save.attr('disabled', '');
+    $.post('/save', { token: token, commentId: commentId, value: value })
+    .then(function(res){
+      console.log('=>', res);
+      // TODO: make sure that res.ok
+      $editor.removeClass('has-changed');
+      refreshLastSavedTime();
+    }, function() {
+      console.log('=> FAIL:', arguments);
+    }).always(function() {
+      $editor.removeClass('is-saving');
+      $save.attr('disabled', null);
+    });
+
   });
 
-});
+  $input.on('keyup', function() {
+    $editor.addClass('has-changed');
+  });
 
-$input.on('keyup', function() {
-  $editor.addClass('has-changed');
-});
+  return {
+    refreshLastSavedTime: refreshLastSavedTime,
+  };
+}
