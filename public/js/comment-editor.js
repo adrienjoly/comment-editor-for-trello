fd5c2ca39ee7e6605ca6f1610a2455daafc65065
Adrien Joly <adrien@whyd.com>
2017-07-27 17:03:20 +0200

    feature: re-indentation of selected lines, when pressing tab / shift+tab

--- public/js/comment-editor.js
+++ public/js/comment-editor.js
@@ -40,20 +40,53 @@ function initEditor(token, commentId) {
 
   });
 
-  function insertTextAtCursor(text) {
-    var cursorPos = $input.prop('selectionStart');
-    var v = $input.val();
-    var textBefore = v.substring(0,  cursorPos);
-    var textAfter  = v.substring(cursorPos, v.length);
-    $input.val(textBefore + text + textAfter);
+  // returns the position of the last line-break before (or at) position
+  function findPosOfLine(text, position) {
+    return position - text.substr(0, position).split('\n').pop().length;
+  }
+  /*
+  // unit tests
+  assert.equal(findPosOfLine('a\nb\nc', 0), 0);
+  assert.equal(findPosOfLine('a\nb\nc', 1), 0);
+  assert.equal(findPosOfLine('a\nb\nc', 2), 2);
+  assert.equal(findPosOfLine('a\nb\nc', 3), 2);
+  assert.equal(findPosOfLine('a\nb\nc', 4), 4);
+  assert.equal(findPosOfLine('a\nb\nc', 5), 4);
+  assert.equal(findPosOfLine('a\nb\nc', 6), 5);
+  */
+
+  // returns text with indented lines between start and end positions
+  function applyIndents(text, start, end, indent) {
+    // 1. select text from 1st line to end
+    var firstLinePos = findPosOfLine(text, start);
+    var range = '\n' + text.substring(firstLinePos, end);
+    // 2. change indentation of lines in that range
+    var output;
+    if (indent === 1) {
+      output = range.replace(/\n/g, '\n\t'); // prepend every line with a tab
+    } else if (indent === -1) {
+      output = range.replace(/\n\t/g, '\n');
+    }
+    return text.substr(0, firstLinePos)
+      + output.substr(1) // remove the '\n' prefix, added for simpler algorithm
+      + text.substr(end);
   }
 
   $input.on('keydown', function(evt) {
-    if(evt.keyCode == TAB_KEY_CODE) {
-      insertTextAtCursor('\t');
-      // TODO: after checking that tab is persisted in trello's db, put it at start of line instead
-      evt.preventDefault();
-      return false;
+    if (evt.key === 'Tab' || evt.keyCode === TAB_KEY_CODE) { // (i) keyCode is deprecated
+      evt.preventDefault && evt.preventDefault();
+      // if tab was pressed, we are going to update the level of indentation
+      // of the selected lines, and leave the selection range as is.
+      var indent = evt.shiftKey ? -1 : 1,
+          input = $input.val(),
+          start = $input.prop('selectionStart'),
+          end = $input.prop('selectionEnd');
+      var output = applyIndents(input, start, end, indent);
+      $input
+        .val(output)
+        .prop('selectionStart', start + indent)
+        .prop('selectionEnd', end + (output.length - input.length));
+      return false; // in case evt.preventDefault() was not effective
     }
   });
 
