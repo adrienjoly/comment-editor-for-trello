c6cc564d18758521cf5307a46b0c90c8e975b3e0
Adrien Joly <adrien@whyd.com>
2017-08-02 10:25:25 +0200

    new index: display README.md

---
+++ docs/index-markdown-render.js
@@ -0,0 +1,37 @@
+// thank you, https://github.com/sawmac/display-markdown
+
+function renderMd(file, MD_URL_PREFIX) {
+
+  var file = file || "README.md";
+  var reader = new stmd.DocParser();
+  var writer = new stmd.HtmlRenderer();
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+      display(xhr);
+    }
+  };
+
+  function display(xhr) {
+    var markdown = xhr.responseText;
+    // transform relative github-hosted markdown links into...
+    //markdown = markdown.replace(/\(([^\.\(\)]+\.md)\)/g, '(' + MD_URL_PREFIX + '$1)'); // ... absolute links to github
+    //markdown = markdown.replace(/\(([^\.\(\)]+\.md)\)/g, '(javascript:renderMd("$1");)'); // ... javascript calls to renderMd()
+    markdown = markdown.replace(/\(([^\.\(\)]+\.md)\)/g, '(#$1)'); // ... hash-links (see window.onhashchange handler on index.html)
+    var parsed = reader.parse(markdown);
+    var content = writer.renderBlock(parsed);
+    document.getElementById('content').innerHTML = content;
+    
+    /* try to extract h1 title and use as title for page
+       if no h1, use name of file 
+    */
+    try {
+      document.title = document.querySelector('h1').textContent
+    } catch (e) {
+      document.title = file;
+    }
+  }
+
+  xhr.open('GET', MD_URL_PREFIX + file);
+  xhr.send();
+}
